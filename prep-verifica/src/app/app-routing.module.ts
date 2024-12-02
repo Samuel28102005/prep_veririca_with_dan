60137b53e2d9aa4331c0a5cbc4ff74826fe3b960
Samuel28102005 <labarba.samuel@iisgalvanimi.edu.it>
2024-12-02 12:51:04 +0000

    inizio

---
+++ prep-verifica/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { ItemsComponent } from './items/items.component';
+
+const routes: Routes = [
+  {path:'search', component:SearchComponent},
+  {path:'items/:id', component:ItemsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ prep-verifica/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ItemsComponent } from './items/items.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    ItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
